71b99f5dec192f85a3f56fbcf47901d47d632f00
Jake Lee <github@jakelee.co.uk>
2023-08-15 21:47:29 +0100

    Variable name tidying

--- automation/youtube-update.sh
+++ automation/youtube-update.sh
@@ -4,14 +4,14 @@ HEADER_PREFIX="#### "
 OUTPUT=""
 
 # Convert list of channels into Markdown tables
-while read -r line; do
-    if [[ $line == $HEADER_PREFIX* ]]; then
-        echo "Adding header $line"
-        OUTPUT="$OUTPUT\n$line\n\n"
+while read -r LINE; do
+    if [[ $LINE == $HEADER_PREFIX* ]]; then
+        echo "Adding header $LINE"
+        OUTPUT="$OUTPUT\n$LINE\n\n"
         OUTPUT="$OUTPUT| Channel | # Videos | Subscribers | Views |\n| --- | --- | --- | --- |\n"
     else
-        IFS=';' read -r -a ARRAY_LINE <<< "$line" # Split line by semi-colon
-        echo "Adding channel ${ARRAY_LINE[1]}"
+        IFS=';' read -r -a ARRAY_LINE <<< "$LINE" # Split line by semi-colon
+        echo "Adding channel ${ARRAY_LINE[1]} (${ARRAY_LINE[0]})"
         curl "https://youtube.googleapis.com/youtube/v3/channels?part=statistics,snippet&id=${ARRAY_LINE[0]}&key=$API_KEY" \
             --header 'Accept: application/json' \
             --silent \
@@ -23,18 +23,14 @@ while read -r line; do
         VIDEO_COUNT=$(jq -r '.items[0].statistics.videoCount' output.json)
         SUBSCRIBER_COUNT=$((($(jq -r '.items[0].statistics.subscriberCount' output.json)+999)/1000))
         VIEW_COUNT=$((($(jq -r '.items[0].statistics.viewCount' output.json)+999999)/1000000))
-        echo "$TITLE: $VIDEO_COUNT videos (${VIEW_COUNT}M views)"
+        echo "Added $TITLE: $VIDEO_COUNT videos (${VIEW_COUNT}M views)"
         OUTPUT="$OUTPUT| ${ARRAY_LINE[2]}[$TITLE](https://youtube.com/$URL) | $VIDEO_COUNT | ${SUBSCRIBER_COUNT}K | ${VIEW_COUNT}M |\n"
     fi
 done < "$WORKSPACE/automation/channels.txt"
 
-# Save Markdown into output file
-echo "$OUTPUT" > temp.md
-
-# Replace placeholder in template with output file's contents, updating the README
-templ=$(<"$WORKSPACE/automation/template.md")
-value=$(<"$WORKSPACE/temp.md")
-echo -e "${templ//dynamic-channel-data/$value}" > "$WORKSPACE/README.md"
+# Replace placeholder in template with output, updating the README
+TEMPLATE_CONTENTS=$(<"$WORKSPACE/automation/template.md")
+echo -e "${TEMPLATE_CONTENTS//dynamic-channel-data/$OUTPUT}" > "$WORKSPACE/README.md"
 
 # Debug
 cat "$WORKSPACE/README.md"
