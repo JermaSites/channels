b4c10703719242bbe3e861f5ea93543ea487e8e6
Jake Lee <github@jakelee.co.uk>
2023-09-09 15:27:50 +0100

    Update youtube-update.sh

--- automation/youtube-update.sh
+++ automation/youtube-update.sh
@@ -1,41 +1,59 @@
 #!/bin/bash
 
-HEADER_PREFIX="#### "
-PLACEHOLDER_TEXT="dynamic-channel-data"
-OUTPUT=""
+channels_file="${WORKSPACE}/automation/channels.txt"
+template_file="${WORKSPACE}/automation/template.md"
+output_file="${WORKSPACE}/README.md"
+
+header_prefix="#### "
+placeholder_text="dynamic-channel-data"
+temp_output_file="output.json"
+output=""
 
 # Convert list of channels into Markdown tables
-while read -r LINE; do
-    if [[ ${LINE} == ${HEADER_PREFIX}* ]]; then
-        echo "Adding header ${LINE}"
-        OUTPUT="${OUTPUT}\n${LINE}\n\n"
-        OUTPUT="${OUTPUT}| Channel ↕ | # Videos ↕ | Subscribers ↕ | Views ↕ |\n| --- | --- | --- | --- |\n"
+while read -r line; do
+    if [[ ${line} == ${header_prefix}* ]]; then
+        echo "Adding header ${line}"
+        output="${output}\n${line}\n\n"
+        output="${output}| Channel ↕ | # Videos ↕ | Subscribers ↕ | Views ↕ |\n| --- | --- | --- | --- |\n"
     else
-        IFS=';' read -r -a ARRAY_LINE <<< "${LINE}" # Split line by semi-colon
-        echo "Adding channel ${ARRAY_LINE[1]} (${ARRAY_LINE[0]})"
-        curl "https://youtube.googleapis.com/youtube/v3/channels?part=statistics,snippet&id=${ARRAY_LINE[0]}&key=${API_KEY}" \
+        IFS=';' read -r channel_id channel_name emoji <<< "${line}" # Split line by semi-colon
+        echo "Adding channel ${channel_name} (${channel_id})"
+        curl "https://youtube.googleapis.com/youtube/v3/channels?part=statistics,snippet&id=${channel_id}&key=${API_KEY}" \
             --header 'Accept: application/json' \
-            -fsSL -o output.json
+            -fsSL -o ${temp_output_file}
 
         # Pull channel data out of response if possible
         if [[ $(jq -r '.pageInfo.totalResults' output.json) == 1 ]]; then
-            TITLE=$(jq -r '.items[0].snippet.title' output.json)
-            URL=$(jq -r '.items[0].snippet.customUrl' output.json)
-            VIDEO_COUNT=$(jq -r '.items[0].statistics.videoCount' output.json | numfmt --to=si)
-            SUBSCRIBER_COUNT=$(jq -r '.items[0].statistics.subscriberCount' output.json | numfmt --to=si)
-            VIEW_COUNT=$(jq -r '.items[0].statistics.viewCount' output.json | numfmt --to=si)
-            echo "Added ${TITLE}: ${VIDEO_COUNT} videos (${VIEW_COUNT} views)"
-            OUTPUT="${OUTPUT}| ${ARRAY_LINE[2]}[${TITLE}](https://youtube.com/${URL}) | ${VIDEO_COUNT} | ${SUBSCRIBER_COUNT} | ${VIEW_COUNT} |\n"
+            jq_fields=(
+                '.items[0].snippet.title'
+                '.items[0].snippet.customUrl'
+                '.items[0].statistics.videoCount'
+                '.items[0].statistics.subscriberCount'
+                '.items[0].statistics.viewCount'
+            )
+            {
+                read -r title
+                read -r url
+                read -r video_count
+                read -r subscriber_count
+                read -r view_count
+            } < <(IFS=','; jq -r "${jq_fields[*]}" < ${temp_output_file})
+
+            video_count=$(numfmt --to=si "${video_count}" | tr G B)
+            subscriber_count=$(numfmt --to=si "${subscriber_count}" | tr G B)
+            view_count=$(numfmt --to=si "${view_count}" | tr G B)
+            echo "Added ${title}: ${video_count} videos (${view_count} views)"
+            output="${output}| ${emoji}[${title}](https://youtube.com/${url}) | ${video_count} | ${subscriber_count} | ${view_count} |\n"
         else
-            echo "Failed! Bad response received: $(<output.json)"
+            echo "Failed! Bad response received: $(<${temp_output_file})"
             exit 1
         fi
     fi
-done < "${WORKSPACE}/automation/channels.txt"
+done < ${channels_file}
 
 # Replace placeholder in template with output, updating the README
-TEMPLATE_CONTENTS=$(<"${WORKSPACE}/automation/template.md")
-echo -e "${TEMPLATE_CONTENTS//${PLACEHOLDER_TEXT}/${OUTPUT}}" > "${WORKSPACE}/README.md"
+template_contents=$(<${template_file})
+echo -e "${template_contents//${placeholder_text}/${output}}" > ${output_file}
 
 # Debug
 cat "${WORKSPACE}/README.md"
